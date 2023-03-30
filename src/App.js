7c5ea100cf8d666b4c9c5766f8c58bd39cfab8e5
dpatel554 <dpatel554@gmail.com>
2023-03-30 10:29:57 -0500

    making changes to Index page

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React the easy way in 4 weeks
         </a>
       </header>
     </div>
